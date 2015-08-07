15211b1e3bd536d83b52cb7d25e036c1e5fed1ec
Jamaleh <Jamaleh@hotmail.com>
2015-08-08 09:27:58 +1000

    user authentication from scratch

--- Gemfile
+++ Gemfile
@@ -52,3 +52,4 @@ end
 
 gem 'bootstrap-sass'
 gem 'active_model_serializers'
+gem 'bcrypt'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807222540) do
+ActiveRecord::Schema.define(version: 20150807231222) do
 
   create_table "items", force: :cascade do |t|
     t.integer  "list_id"
@@ -35,9 +35,11 @@ ActiveRecord::Schema.define(version: 20150807222540) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "password"
+    t.string   "password_hash"
+    t.string   "password_salt"
   end
 
 end
