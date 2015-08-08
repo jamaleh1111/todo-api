191ce15a23bda061eeea3cbb7fad93610ef390df
Jamaleh <Jamaleh@hotmail.com>
2015-08-08 16:27:40 +1000

    add update to items route

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   end 
 
   resources :lists, only: [] do
-    resources :items, only: [:create]
+    resources :items, only: [:create, :update]
   end 
-  resources :items, only: [:destroy]
+  resources :items, only: [:destroy, :update]
 end 
 end 
 
