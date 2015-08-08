a3b2228e7bb51b9f1f008d602eeeab678e3b6b6f
Jamaleh <Jamaleh@hotmail.com>
2015-08-08 14:24:53 +1000

    working on user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "session#new", :as => "log_in"
-  get "sign_up" => "users#new", :as => "sign_up"
-  resources :sessions
+ 
 
   root to: 'welcome#index'
   
